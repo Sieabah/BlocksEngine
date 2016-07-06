562c158a5b8e1fb446af479b21924c776c8e9e7f
sieabah <christopher.sidell@gmail.com>
2016-07-05 22:25:44 -0400

    [GB2-1] Add pixi and soundmanger

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     uglify = require('gulp-uglify'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    concat = require('gulp-concat');
 
-gulp.task('game', function(){
+gulp.task('packages', function(){
+    return gulp.src('src/packages/**/*.js')
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('built/local'));
+});
+
+gulp.task('game', ['packages'], function(){
     return gulp.src('src/game/**/*.ts')
         .pipe(ts({
             target: 'ES5',
@@ -13,7 +20,7 @@ gulp.task('game', function(){
 });
 
 gulp.task('prod', ['game'], function () {
-    return gulp.src('built/local/game.js')
+    return gulp.src(['built/local/game.js','built/local/vendor.js'])
         .pipe(uglify())
         .pipe(gulp.dest('built/prod'));
 });
